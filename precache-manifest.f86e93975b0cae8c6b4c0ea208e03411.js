21b3904e5d0fbc981289e155d12f7847d2c6474c
Dipta Biswas <46165735+Charlygraphy23@users.noreply.github.com>
2020-08-12 16:57:22 +0530

    Add files via upload

---
+++ precache-manifest.f86e93975b0cae8c6b4c0ea208e03411.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9ce882f7bb6a1593e39fc3bdf7cd07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc0fbdddf97a22d8bf4",
+    "url": "/static/css/main.2c7ad196.chunk.css"
+  },
+  {
+    "revision": "f56b30828ba5a5daaff5",
+    "url": "/static/js/2.5b3490e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5b3490e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc0fbdddf97a22d8bf4",
+    "url": "/static/js/main.f92af3e7.chunk.js"
+  },
+  {
+    "revision": "69fbc72b0a1edc342a9f",
+    "url": "/static/js/runtime-main.6021ed92.js"
+  },
+  {
+    "revision": "a249da802ac5ce646ced4d7b8b43e379",
+    "url": "/static/media/clearmoon.a249da80.svg"
+  },
+  {
+    "revision": "b332e91757a0fb2e22253bf5268495d3",
+    "url": "/static/media/cloud.b332e917.svg"
+  },
+  {
+    "revision": "be5e5d1442ee7d03987183805e5e7714",
+    "url": "/static/media/dreary.be5e5d14.svg"
+  },
+  {
+    "revision": "b9430259402b984af1350f010a3e0de4",
+    "url": "/static/media/foggy.b9430259.svg"
+  },
+  {
+    "revision": "486394a31c540f40afdeb8915102f641",
+    "url": "/static/media/hazy.486394a3.svg"
+  },
+  {
+    "revision": "8014033a33f681b9bdd23aa50c22136a",
+    "url": "/static/media/hazyMoon.8014033a.svg"
+  },
+  {
+    "revision": "22bc0a3641b912ef76a5829c33b87994",
+    "url": "/static/media/logoWeather.22bc0a36.png"
+  },
+  {
+    "revision": "a4acb814cd4db21bee154be0bfcc72ab",
+    "url": "/static/media/moonthunderstorm.a4acb814.svg"
+  },
+  {
+    "revision": "d6ad10278f5871c68b0622477c15a547",
+    "url": "/static/media/rain.d6ad1027.svg"
+  },
+  {
+    "revision": "fff3ba14e66338a5b50f862351c39bd3",
+    "url": "/static/media/search.fff3ba14.svg"
+  },
+  {
+    "revision": "2fe289a54f32148a1b6d10ffb5600ef4",
+    "url": "/static/media/searchIcon.2fe289a5.png"
+  },
+  {
+    "revision": "cae6e0b405f905899129dd135bb9b4ff",
+    "url": "/static/media/snow.cae6e0b4.svg"
+  },
+  {
+    "revision": "a214b750bb7c08f37c8e3751534d12db",
+    "url": "/static/media/sunny.a214b750.svg"
+  },
+  {
+    "revision": "5165477f6f1da6fe42a36cf72c1fc26a",
+    "url": "/static/media/thunderstorm.5165477f.svg"
+  }
+]);
